d79c442ad9d40b5eacc088fdbb21a9d0cabf1e44
Erick Gallesio <eg at unice.fr>
2018-04-20 08:47:20 +0200

    Updated for STklos version 1.20

--- stklos/src/stklos-posix.c
+++ stklos/src/stklos-posix.c
@@ -1,30 +1,32 @@
-/*
- * stklos-posix.c			-- Interface for POSIX
- * 
- * Copyright © 2005-2007 Erick Gallesio - I3S-CNRS/ESSI <eg@essi.fr>
- * 
- * 
+/*                                              -*- coding: utf-8 -*-
+ *
+ * stklos-posix.c                       -- Interface for POSIX
+ *
+ * Copyright Â© 2005-2018 Erick Gallesio - I3S-CNRS/ESSI <eg@essi.fr>
+ *
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
  * USA.
- * 
+ *
  *           Author: Erick Gallesio [eg@essi.fr]
  *    Creation date: 15-Dec-2005 11:52 (eg)
- * Last file update: 30-Jun-2007 23:52 (eg)
+ * Last file update: 12-Apr-2018 17:10 (eg)
  */
 #include <stklos.h>
 #include <sys/stat.h>
+#include <ctype.h>
 
 static void strlower(char *s)
 {
@@ -36,33 +38,33 @@ static void strlower(char *s)
 static void defvar(char *name, long value, SCM module)
 {
   char buff[100];
-  
+
   snprintf(buff, 100, "posix/%s", name);
   strlower(buff+6);
 
   STk_define_variable(STk_intern(buff),
-		      MAKE_INT(value), 
-		      module);
+                      MAKE_INT(value),
+                      module);
 }
 
-static error_bad_path_name(SCM path)
+static void error_bad_path_name(SCM path)
 {
   STk_error("bad path name ~S", path);
 }
 
-static error_bad_parameter(SCM obj)
+static void error_bad_parameter(SCM obj)
 {
   STk_error("bad parameter ~S", obj);
 }
 
-static error_user_informations(SCM who)
+static void error_user_informations(SCM who)
 {
   STk_error("cannot find user informations for ~S", who);
 }
 
 
 /* ----------------------------------------------------------------------
- * 	Directories functions ...
+ *      Directories functions ...
  * ----------------------------------------------------------------------  */
 DEFINE_PRIMITIVE("posix-make-directory", posix_make_directory, subr1, (SCM path))
 {
@@ -108,7 +110,7 @@ DEFINE_PRIMITIVE("posix-current-directory", get_dir, subr0, (void))
 
 
 /* ----------------------------------------------------------------------
- * 	Links ...
+ *      Links ...
  * ----------------------------------------------------------------------  */
 DEFINE_PRIMITIVE("posix-make-link", make_link, subr2, (SCM old, SCM new))
 {
@@ -116,7 +118,7 @@ DEFINE_PRIMITIVE("posix-make-link", make_link, subr2, (SCM old, SCM new))
 
   if (!STRINGP(old)) error_bad_path_name(old);
   if (!STRINGP(new)) error_bad_path_name(new);
-  
+
   n = link(STRING_CHARS(old), STRING_CHARS(new));
  return MAKE_BOOLEAN( n == 0 );
 }
@@ -127,7 +129,7 @@ DEFINE_PRIMITIVE("posix-make-symbolic-link", make_slink, subr2, (SCM old, SCM ne
 
   if (!STRINGP(old)) error_bad_path_name(old);
   if (!STRINGP(new)) error_bad_path_name(new);
-  
+
   n = symlink(STRING_CHARS(old), STRING_CHARS(new));
   return MAKE_BOOLEAN( n == 0 );
 }
@@ -148,31 +150,31 @@ DEFINE_PRIMITIVE("posix-read-symbolic-link", readslink, subr1, (SCM path))
 {
   int n;
   char buff[MAX_PATH_LENGTH];
-  
+
   if (!STRINGP(path)) error_bad_path_name(path);
   n = readlink(STRING_CHARS(path), buff, MAX_PATH_LENGTH);
-  
-  if (n == -1) 
+
+  if (n == -1)
     return STk_false;
   else {
     SCM z = STk_makestring(n, NULL);
-    
+
     memcpy(STRING_CHARS(z), buff, n);
     return z;
   }
 }
-  
+
 
 
 /* ----------------------------------------------------------------------
- * 	Errno ...
+ *      Errno ...
  * ----------------------------------------------------------------------  */
 #define DEF_ERRNO(x)     {defvar(#x, x, module);}
 
 static SCM posix_errno_conv(SCM value)
 {
   long val = STk_integer_value(value);
-  
+
   if (val == LONG_MIN) error_bad_parameter(value);
   errno = val;
   return value;
@@ -436,7 +438,7 @@ DEFINE_PRIMITIVE("posix-error", posix_error, subr0, (void))
 }
 
 /* ----------------------------------------------------------------------
- * 	Informations ...
+ *      Informations ...
  * ----------------------------------------------------------------------  */
 #include <pwd.h>
 #include <grp.h>
@@ -450,8 +452,8 @@ DEFINE_PRIMITIVE("%posix-file-informations", file_stat, subr1, (SCM path))
   if (!STRINGP(path)) error_bad_path_name(path);
   n = lstat(STRING_CHARS(path), &info);
   if (n) return STk_false;
-  
-  z = STk_makevect(10, NULL);	/* NIL because some integers are bignums */
+
+  z = STk_makevect(10, NULL);   /* NIL because some integers are bignums */
   VECTOR_DATA(z)[0] = STk_long2integer((long)info.st_dev);
   VECTOR_DATA(z)[1] = STk_long2integer((long)info.st_ino);
   VECTOR_DATA(z)[2] = STk_long2integer((long)info.st_mode);
@@ -464,7 +466,7 @@ DEFINE_PRIMITIVE("%posix-file-informations", file_stat, subr1, (SCM path))
   VECTOR_DATA(z)[9] = STk_long2integer((long)info.st_ctime);
   return z;
 }
-  
+
 
 DEFINE_PRIMITIVE("posix-file-permissions", file_permissions, subr1, (SCM path))
 {
@@ -581,10 +583,10 @@ static void init_posix_modes(SCM module)
 
 
 /* ----------------------------------------------------------------------
- * 
- * 	Module posix starts here
  *
- * ---------------------------------------------------------------------- 
+ *      Module posix starts here
+ *
+ * ----------------------------------------------------------------------
  */
 #include "stklos-posix-inc.c"
 
@@ -606,7 +608,7 @@ MODULE_ENTRY_START("stklos-posix")
   ADD_PRIMITIVE_IN_MODULE(make_slink, module);
   ADD_PRIMITIVE_IN_MODULE(symlinkp, module);
   ADD_PRIMITIVE_IN_MODULE(readslink, module);
-    
+
 
   ADD_PRIMITIVE_IN_MODULE(file_permissions, module);
   ADD_PRIMITIVE_IN_MODULE(file_stat, module);
@@ -623,7 +625,7 @@ MODULE_ENTRY_START("stklos-posix")
 
   /* Execute Scheme code */
   STk_execute_C_bytecode(__module_consts, __module_code);
-} MODULE_ENTRY_END 
+} MODULE_ENTRY_END
 
 
 MODULE_ENTRY_INFO() {
